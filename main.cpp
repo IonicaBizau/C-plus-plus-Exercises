edcde53d9c8be639f2e27a5672ea400d2f031588
Ionică Bizău <bizauionica@yahoo.com>
2013-04-24 10:48:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+    II. 1. a
+        2. c
+        3. s1 == 'b' && s2 == 'u' &&  s3 == 'n'  
+        4.
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -66,3 +73,80 @@ int main()
 
     return 0;
 }
+
+/*
+    III. 1. a
+         2.
+        #include <iostream>
+
+        using namespace std;
+        
+        int array[] = {
+            49, 23, 21, 17, 12, 7, 5
+        };
+        
+        int search (int s, int d, int x) {
+        
+            if(s > d) {
+                return -1;
+            }
+        
+            int m =(s + d)/2;    
+        
+            if (x == array[m]) {
+                return m;
+            }
+        
+            if (x < array[m]) {
+                return search(m + 1, d, x);
+            }
+        
+            return search(s, m - 1, x);
+        }
+        
+        int main() {
+        
+            cout << search(0, 8, 21);
+        
+            return 0;
+        }
+
+        3. TODO
+        
+        #include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "n = ";
+    cin >> n;
+
+    if (n > 20 || n < 2) {
+        cout << "error.";
+        return 0;
+    }
+
+    int array[20];
+
+    for (int i = 0; i < n; i++) {
+        cout << "array[" << i << "] =";
+        cin >> array[i];
+    }
+
+    int array2[40];
+    
+    int i = 0;
+    int l = 0;
+
+    while (true) {
+        if (array[i] % 2 == 0) {
+        
+        }
+    }
+    
+    return 0;
+}
+
+        
+*/
